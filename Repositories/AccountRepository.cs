44f010d8028d1141a6285e9f08baf034de37bd30
baokhang46 <144251046+baokhang46@users.noreply.github.com>
2024-06-08 14:24:58 +0700

    Account Repo&Service

--- Repositories/AccountRepository.cs
+++ Repositories/AccountRepository.cs
@@ -8,9 +8,20 @@ using System.Threading.Tasks;
 
 namespace Repositories
 {
-    public class AccountRepository
+    public class AccountRepository : IAccountRepository
     {
         public Account GetAccountByEmail(string email)
             => AccountDAO.GetAccountByEmail(email);
+        public List<Account> GetSystemAccounts()
+            => AccountDAO.GetSystemAccounts();
+        public Account GetAccountById(int accountId)
+            => AccountDAO.GetAccountById(accountId);
+        public void CreateAccount(Account account)
+            => AccountDAO.CreateAccount(account);
+        public void UpdateAccount(Account account)
+            => AccountDAO.UpdateAccount(account);
+        public void DeactivateSystemAccount(int accountId)
+            => AccountDAO.DeactivateSystemAccount(accountId);
+
     }
 }
--- Services/IAccountService.cs
+++ Services/IAccountService.cs
@@ -5,5 +5,10 @@ namespace Services
     public interface IAccountService
     {
         Account GetAccountByEmail(string email);
+        List<Account> GetSystemAccounts();
+        Account GetAccountById(int accountId);
+        void CreateAccount(Account account);
+        void UpdateAccount(Account account);
+        void DeactivateSystemAccount(int accountId);
     }
 }
